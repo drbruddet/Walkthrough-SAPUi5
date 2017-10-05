ed6dc654a636829c6818193c1ad579768df02cf6
guillaumeko <guillaume.kolly@gmail.com>
2017-10-05 15:10:10 +0000

    application is getting responsive and adapted to devices

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/demo/wt/controller/HelloDialog"
-], function (UIComponent, JSONModel, HelloDialog) {
+	"sap/ui/demo/wt/controller/HelloDialog",
+	"sap/ui/Device"
+], function (UIComponent, JSONModel, HelloDialog, Device) {
 	"use strict";
 	return UIComponent.extend("sap.ui.demo.wt.Component", {
 		
@@ -22,12 +23,28 @@ sap.ui.define([
         	var oModel = new JSONModel(oData);
         	this.setModel(oModel);
         	
+        	// set device model
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
+        	
         	// set dialog
 			this._helloDialog = new HelloDialog(this.getRootControl());
 			
 			// create the views based on the url/hash
 			this.getRouter().initialize();
     	},
+    	
+    	getContentDensityClass : function() {
+			if (!this._sContentDensityClass) {
+				if (!sap.ui.Device.support.touch) {
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
+		},
     	
     	openHelloDialog : function () {
 			this._helloDialog.open();
